41de39f80d272ae7b2697dffa7a99258ee66ad58
Roman Buckle <rb10g16@soton.ac.uk>
2019-03-30 16:10:32 +0000

    first commit

---
+++ rhex_controller/include/rhex_controller/rhex_controller_simple.hpp
@@ -0,0 +1,139 @@
+#ifndef RHEX_CONTROLLER_RHEX_CONTROLLER_SIMPLE_HPP
+#define RHEX_CONTROLLER_RHEX_CONTROLLER_SIMPLE_HPP
+
+// For M_PI constant
+#define _USE_MATH_DEFINES
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <vector>
+#define PI 3.14159265
+#define ARRAY_DIM 100
+
+namespace rhex_controller {
+
+    class RhexControllerSimple {
+    public:
+        typedef std::array<double, ARRAY_DIM> array_t;
+
+        RhexControllerSimple() {}
+        RhexControllerSimple(const std::vector<double>& ctrl, std::vector<int> broken_legs)
+            : _broken_legs(broken_legs)
+        {
+            set_parameters(ctrl);
+
+            if (_Kp.size() == 0)
+                set_pd(5., 0.1);
+        }
+
+        void set_parameters(const std::vector<double>& ctrl)
+        {
+            //assert(ctrl.size() == 48);
+
+
+            _controller = ctrl;
+
+
+        }
+        void set_pd(double Kp, double Kd)
+        {
+        	_Kp.clear();
+        	_Kd.clear();
+            for(int i=0; i<54 ;i++){
+            	_Kp.push_back(Kp);
+            	_Kd.push_back(Kd);
+            }
+        }
+
+        const std::vector<double>& parameters() const
+        {
+            return _controller;
+        }
+
+        void set_broken(const std::vector<int> broken_legs)
+        {
+            _broken_legs = broken_legs;
+        }
+
+        const std::vector<int>& broken_legs() const
+        {
+            return _broken_legs;
+        }
+
+        std::vector<double>  pos(double t) 
+        {
+        	
+            //assert(_controller.size() == 48);
+
+            double ratio = 0;
+            double help = 0;
+            double temp = 0;
+            double other =0;
+            help = remainder(double(t),double(0.75))/(0.75);
+            help = help +0.5;
+            
+            
+            ratio = (help<_controller[0])? help*_controller[1]*2 : _controller[1]+(help-_controller[0])*(1-_controller[1])*2;
+            ratio = ratio +((1-_controller[1])/2);
+            if(ratio>1){
+                ratio = ratio-1;
+            }
+            
+
+            temp = ((help+_controller[4])>1)? help-_controller[4] : help+_controller[4];
+            other = (temp<_controller[2])? temp*_controller[3]*2 : _controller[3]+(temp-_controller[2])*(1-_controller[3])*2;
+            other += ((1-_controller[3])/2);
+            if (other>1){
+                other = other-1;
+            }
+            
+
+            //std::cout<<ratio<<"--"<<other<<std::endl;
+
+//front middle back left/right
+            std::vector<double> tau(48,0);
+               
+            
+
+            for(int i = 0; i < 6; i++){
+                if((i%2) == 0){
+               		tau[i]= ratio*2*PI;
+                }else{
+                    tau[i]= other*2*PI;
+                   }
+            }
+            
+
+
+            set_pd(_controller[5], _controller[6]);
+
+            if (help>0.5){
+                _Kp[0+6] = _controller[7];
+                _Kp[2+6] = _controller[7];
+                _Kp[4+6] = _controller[7];
+            }
+            if (temp>0.5){
+                _Kp[1+6] = _controller[7];
+                _Kp[3+6] = _controller[7];
+                _Kp[5+6] = _controller[7];
+            }
+            //std::cout<<tau[0]<<" "<<tau[1]<<" "<<tau[2]<<" "<<tau[3]<<" "<<tau[4]<<" "<<tau[5]<<std::endl;
+
+            return tau;
+        }
+        std::vector<double> get_Kp(void){
+        	return _Kp;
+        }
+        std::vector<double> get_Kd(void){
+        	return _Kd;
+        }
+
+    protected:
+        std::vector<double> _controller;
+        std::vector<int> _broken_legs;
+        std::vector<double> _Kp;
+        std::vector<double> _Kd;
+    };
+} // namespace rhex_controller
+
+#endif
---
+++ rhex_controller/src/rhex_controller_simple.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <rhex_controller/rhex_controller_simple.hpp>
+
+using namespace rhex_controller;
+
+int main()
+{
+    RhexControllerSimple controller({1, 0, 0.5, 0.25, 0.25, 0.5, 1, 0.5, 0.5, 0.25, 0.75, 0.5, 1, 0, 0.5, 0.25, 0.25, 0.5, 1, 0, 0.5, 0.25, 0.75, 0.5, 1, 0.5, 0.5, 0.25, 0.25, 0.5, 1, 0, 0.5, 0.25, 0.75, 0.5}, {});
+
+    for (double t = 0.0; t <= 5.0; t += 0.1)
+        auto angles = controller.pos(t);
+    return 0;
+}
